3af00d8eff98c03c6b73924ab98b82a0b771af1b
Michael Sartain <mikesart@fastmail.com>
2017-10-11 17:59:57 -0600

    trace-cmd start cleanup

--- sample/trace-cmd-start-tracing.sh
+++ sample/trace-cmd-start-tracing.sh
@@ -56,38 +56,40 @@ fi
 EVENTS=
 
 # https://github.com/mikesart/gpuvis/wiki/TechDocs-Linux-Scheduler
-EVENTS="$EVENTS -e 'sched:sched_switch'"
-EVENTS="$EVENTS -e 'sched:sched_process_fork'"
-EVENTS="$EVENTS -e 'sched:sched_process_exec'"
-EVENTS="$EVENTS -e 'sched:sched_process_exit'"
+EVENTS="$EVENTS -e sched:sched_switch"
+EVENTS="$EVENTS -e sched:sched_process_fork"
+EVENTS="$EVENTS -e sched:sched_process_exec"
+EVENTS="$EVENTS -e sched:sched_process_exit"
 
-EVENTS="$EVENTS -e 'drm:drm_vblank_event'"
-EVENTS="$EVENTS -e 'drm:drm_vblank_event_queued'"
+EVENTS="$EVENTS -e drm:drm_vblank_event"
+EVENTS="$EVENTS -e drm:drm_vblank_event_queued"
 
 # https://github.com/mikesart/gpuvis/wiki/TechDocs-AMDGpu
-EVENTS="$EVENTS -e 'amdgpu:amdgpu_vm_flush'"
-EVENTS="$EVENTS -e 'amdgpu:amdgpu_cs_ioctl'"
-EVENTS="$EVENTS -e 'amdgpu:amdgpu_sched_run_job'"
-EVENTS="$EVENTS -e '*fence:*fence_signaled'"
+EVENTS="$EVENTS -e amdgpu:amdgpu_vm_flush"
+EVENTS="$EVENTS -e amdgpu:amdgpu_cs_ioctl"
+EVENTS="$EVENTS -e amdgpu:amdgpu_sched_run_job"
+EVENTS="$EVENTS -e *fence:*fence_signaled"
 
 # https://github.com/mikesart/gpuvis/wiki/TechDocs-Intel
 #
 # NOTE: the i915_gem_request_submit, i915_gem_request_in, i915_gem_request_out
 # tracepoints require the CONFIG_DRM_I915_LOW_LEVEL_TRACEPOINTS Kconfig option to
 # be enabled.
-EVENTS="$EVENTS -e 'i915:i915_flip_request'"
-EVENTS="$EVENTS -e 'i915:i915_flip_complete'"
-EVENTS="$EVENTS -e 'i915:intel_gpu_freq_change'"
-EVENTS="$EVENTS -e 'i915:i915_gem_request_add'"
-EVENTS="$EVENTS -e 'i915:i915_gem_request_submit'"
-EVENTS="$EVENTS -e 'i915:i915_gem_request_in'"
-EVENTS="$EVENTS -e 'i915:i915_gem_request_out'"
-EVENTS="$EVENTS -e 'i915:intel_engine_notify'"
-EVENTS="$EVENTS -e 'i915:i915_gem_request_wait_begin'"
-EVENTS="$EVENTS -e 'i915:i915_gem_request_wait_end'"
+EVENTS="$EVENTS -e i915:i915_flip_request"
+EVENTS="$EVENTS -e i915:i915_flip_complete"
+EVENTS="$EVENTS -e i915:intel_gpu_freq_change"
+EVENTS="$EVENTS -e i915:i915_gem_request_add"
+EVENTS="$EVENTS -e i915:i915_gem_request_submit"
+EVENTS="$EVENTS -e i915:i915_gem_request_in"
+EVENTS="$EVENTS -e i915:i915_gem_request_out"
+EVENTS="$EVENTS -e i915:intel_engine_notify"
+EVENTS="$EVENTS -e i915:i915_gem_request_wait_begin"
+EVENTS="$EVENTS -e i915:i915_gem_request_wait_end"
 
+echo
 CMD="trace-cmd start -b 2000 -i ${EVENTS}"
 echo $CMD
 $CMD
 
+echo
 ./trace-cmd-status.sh
