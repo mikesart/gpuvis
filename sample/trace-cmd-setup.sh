71449f249fc756cb8e32cf172ea17307f89d316f
Michael Sartain <mikesart@fastmail.com>
2017-10-11 22:28:52 -0600

    Add trace-cmd setup bash script

---
+++ sample/trace-cmd-setup.sh
@@ -0,0 +1,102 @@
+#/bin/bash
+
+set -eu
+
+ESC="$(printf '\033')"
+NORMAL="${ESC}[0m"
+YELLOW="${ESC}[1;33m"
+GREEN="${ESC}[32;1m"
+
+function spewTraceStatus() {
+    echo "Tracing status:"
+
+    if [ -u "${TRACECMD}" ]; then
+        echo "  ${TRACECMD} ${GREEN}(setuid set)${NORMAL}"
+    else
+        echo "  ${TRACECMD} ${YELLOW}(setuid not set)${NORMAL}"
+    fi
+
+    if egrep -q "${TRACEFS} .*tracefs" /proc/mounts; then
+        echo "  ${TRACEFS} ${GREEN}(mounted)${NORMAL}"
+    else
+        echo "  ${TRACEFS} ${YELLOW}(not mounted)${NORMAL}"
+    fi
+
+    if [ -w "${TRACEFS}/trace_marker" ]; then
+        echo "  ${TRACEFS}/trace_marker ${GREEN}(writable)${NORMAL}"
+    else
+        echo "  ${TRACEFS}/trace_marker ${YELLOW}(not writable)${NORMAL}"
+    fi
+}
+
+ROOT_CMDS=
+
+# trace-cmd mounts tracefs on /sys/kernel/tracing
+# from /proc/mounts:
+#   nodev /sys/kernel/tracing tracefs rw,relatime 0 0
+TRACEFS="/sys/kernel/tracing"
+
+TRACECMD=$(which trace-cmd)
+
+if [ -z "${TRACECMD}" ]; then
+    echo "ERROR: Could not locate trace-cmd binary"
+    exit -1
+fi
+
+if [ -u "${TRACECMD}" ]; then
+    # trace-cmd setuid set
+
+    # Check if tracefs dir is mounted
+    if ! egrep -q " ${TRACEFS} .*tracefs" /proc/mounts; then
+        # Run 'trace-cmd stat' to mount tracefs
+        echo Mounting ${TRACEFS}...
+        ${TRACECMD} stat > /dev/null 2>&1
+    fi
+else
+    # trace-cmd setuid not set
+
+    ROOT_CMDS+="# Add setuid bit to trace-cmd binary\n"
+    ROOT_CMDS+="chmod u+s ${TRACECMD}\n\n"
+
+    ROOT_CMDS+="# Launch trace-cmd to mount tracefs directory\n"
+    ROOT_CMDS+="${TRACECMD} stat > /dev/null 2>&1\n\n"
+fi
+
+if [ -w "${TRACEFS}/trace_marker" ]; then
+    # trace_marker writable
+    :
+else
+    # trace_marker not writable
+    ROOT_CMDS+="# Add tracefs execute/search permissions\n"
+    ROOT_CMDS+="chmod 0755 \"${TRACEFS}\"\n\n"
+
+    ROOT_CMDS+="# Add trace_marker write permissions \n"
+    ROOT_CMDS+="chmod 0222 \"${TRACEFS}/trace_marker\"\n"
+fi
+
+if [ -z "${ROOT_CMDS}" ]; then
+    :
+else
+    ROOT_CMDS_FILE="$(mktemp)"
+    echo -e ${ROOT_CMDS} > ${ROOT_CMDS_FILE}
+
+    echo ""
+    echo "trace-cmd root initialization file (${ROOT_CMDS_FILE}):"
+    echo "---------------------------------"
+    cat -s -n "${ROOT_CMDS_FILE}"
+    echo ""
+
+    if [ -z "$(which gksudo)" ]; then
+        echo 'xterm -e "echo Enter your password to setup trace-cmd permissions:; sudo bash ${ROOT_CMDS_FILE}"'
+        xterm -e "echo Enter your password to setup trace-cmd permissions:; sudo bash ${ROOT_CMDS_FILE}"
+    else
+        echo 'gksudo --message "Setup trace-cmd permissions" "bash ${ROOT_CMDS_FILE}"'
+        gksudo --message "Setup trace-cmd permissions" "bash ${ROOT_CMDS_FILE}"
+    fi
+
+    echo
+    rm ${ROOT_CMDS_FILE}
+fi
+
+spewTraceStatus
+
--- sample/trace-cmd-start-tracing.sh
+++ sample/trace-cmd-start-tracing.sh
@@ -1,90 +1,49 @@
 #/bin/bash
 
-DEBUGFS=$(grep -w ^debugfs /proc/mounts | awk '{ print $2; }')
-TRACEFS=${DEBUGFS}/tracing
-
 TRACECMD=$(which trace-cmd)
-# TRACECMD=/usr/bin/trace-cmd
-
-ROOT_CMDS=
-
-# User name of owner: stat -c %U /sys/kernel/debug
-
-if [ -z "${DEBUGFS}" ]; then
-    echo "ERROR: Could not locate debugfs directory"
-    exit -1
-fi
 
 if [ -z "${TRACECMD}" ]; then
     echo "ERROR: Could not locate trace-cmd binary"
     exit -1
 fi
 
-echo "debugfs: ${DEBUGFS} (permissions:$(stat -c %a ${DEBUGFS}))"
-echo "tracefs: ${TRACEFS}"
-
-if [ -w "${TRACEFS}/trace_marker" ]; then
-    echo "trace_marker: ${TRACEFS}/trace_marker (writable)"
-else
-    echo "trace_marker: ${TRACEFS}/trace_marker (not writable)"
-
-    ROOT_CMDS="${ROOT_CMDS}chmod 0755 \"${DEBUGFS}\"\n"
-    ROOT_CMDS="${ROOT_CMDS}ls \"${TRACEFS}\" > /dev/null 2>&1\n"
-    ROOT_CMDS="${ROOT_CMDS}chmod 0755 \"${TRACEFS}\"\n"
-    ROOT_CMDS="${ROOT_CMDS}chmod 0222 \"${TRACEFS}/trace_marker\"\n"
-fi
-
-if [ -u "${TRACECMD}" ]; then
-    echo "${TRACECMD}: (setuid set)"
-else
-    echo "${TRACECMD}: (setuid not set)"
-
-    ROOT_CMDS="${ROOT_CMDS}chmod u+s ${TRACECMD}\n"
-fi
-
-if [ -z "${ROOT_CMDS}" ]; then
-    :
-else
-    ROOT_CMDS_FILE="$(mktemp)"
-    echo -e ${ROOT_CMDS} > ${ROOT_CMDS_FILE}
-    echo ROOT_CMDS_FILE is ${ROOT_CMDS_FILE}
-
-    cat "${ROOT_CMDS_FILE}"
-    xterm -e "echo Enter your password to setup SteamVR GPU profiling:; sudo bash ${ROOT_CMDS_FILE}"
+if [ ! -u "${TRACECMD}" ]; then
+    echo "ERROR: ${TRACECMD} setuid not set"
+    exit -1
 fi
 
 EVENTS=
 
 # https://github.com/mikesart/gpuvis/wiki/TechDocs-Linux-Scheduler
-EVENTS="$EVENTS -e sched:sched_switch"
-EVENTS="$EVENTS -e sched:sched_process_fork"
-EVENTS="$EVENTS -e sched:sched_process_exec"
-EVENTS="$EVENTS -e sched:sched_process_exit"
+EVENTS+=" -e sched:sched_switch"
+EVENTS+=" -e sched:sched_process_fork"
+EVENTS+=" -e sched:sched_process_exec"
+EVENTS+=" -e sched:sched_process_exit"
 
-EVENTS="$EVENTS -e drm:drm_vblank_event"
-EVENTS="$EVENTS -e drm:drm_vblank_event_queued"
+EVENTS+=" -e drm:drm_vblank_event"
+EVENTS+=" -e drm:drm_vblank_event_queued"
 
 # https://github.com/mikesart/gpuvis/wiki/TechDocs-AMDGpu
-EVENTS="$EVENTS -e amdgpu:amdgpu_vm_flush"
-EVENTS="$EVENTS -e amdgpu:amdgpu_cs_ioctl"
-EVENTS="$EVENTS -e amdgpu:amdgpu_sched_run_job"
-EVENTS="$EVENTS -e *fence:*fence_signaled"
+EVENTS+=" -e amdgpu:amdgpu_vm_flush"
+EVENTS+=" -e amdgpu:amdgpu_cs_ioctl"
+EVENTS+=" -e amdgpu:amdgpu_sched_run_job"
+EVENTS+=" -e *fence:*fence_signaled"
 
 # https://github.com/mikesart/gpuvis/wiki/TechDocs-Intel
 #
 # NOTE: the i915_gem_request_submit, i915_gem_request_in, i915_gem_request_out
 # tracepoints require the CONFIG_DRM_I915_LOW_LEVEL_TRACEPOINTS Kconfig option to
 # be enabled.
-EVENTS="$EVENTS -e i915:i915_flip_request"
-EVENTS="$EVENTS -e i915:i915_flip_complete"
-EVENTS="$EVENTS -e i915:intel_gpu_freq_change"
-EVENTS="$EVENTS -e i915:i915_gem_request_add"
-EVENTS="$EVENTS -e i915:i915_gem_request_submit"
-EVENTS="$EVENTS -e i915:i915_gem_request_in"
-EVENTS="$EVENTS -e i915:i915_gem_request_out"
-EVENTS="$EVENTS -e i915:intel_engine_notify"
-EVENTS="$EVENTS -e i915:i915_gem_request_wait_begin"
-EVENTS="$EVENTS -e i915:i915_gem_request_wait_end"
+EVENTS+=" -e i915:i915_flip_request"
+EVENTS+=" -e i915:i915_flip_complete"
+EVENTS+=" -e i915:intel_gpu_freq_change"
+EVENTS+=" -e i915:i915_gem_request_add"
+EVENTS+=" -e i915:i915_gem_request_submit"
+EVENTS+=" -e i915:i915_gem_request_in"
+EVENTS+=" -e i915:i915_gem_request_out"
+EVENTS+=" -e i915:intel_engine_notify"
+EVENTS+=" -e i915:i915_gem_request_wait_begin"
+EVENTS+=" -e i915:i915_gem_request_wait_end"
 
 echo
 CMD="trace-cmd start -b 2000 -i ${EVENTS}"
