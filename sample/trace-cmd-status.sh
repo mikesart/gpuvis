719604f9b2c1a9ce041e18872b1cc0c7bf00cd2d
Michael Sartain <mikesart@fastmail.com>
2017-11-03 11:01:38 -0600

    Add entries / overrun counts for tracing from per_cpu stats files

--- sample/trace-cmd-status.sh
+++ sample/trace-cmd-status.sh
@@ -3,3 +3,22 @@
 CMD="trace-cmd stat"
 echo $CMD
 $CMD
+
+STAT_FILES=()
+
+# /sys/kernel/tracing/per_cpu/cpu0/stats
+for (( i = 0 ; i <= 256 ; i++ )) ; do
+    FILE="/sys/kernel/tracing/per_cpu/cpu${i}/stats"
+    if [ ! -r ${FILE} ]; then
+        break
+    fi
+
+    STAT_FILES+=($FILE)
+done
+
+awk '{
+        if ( ( $2 > 0 ) && ( $0 ~ /^(entries\:|overrun\:)/ ) )
+        {
+            printf "%s: %s\n", FILENAME, $0
+        }
+     }' ${STAT_FILES[@]}
