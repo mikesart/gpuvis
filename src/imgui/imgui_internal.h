d9b770d0f562d99eacc0ba5f7b99fd14c1f51fea
omar <omarcornut@gmail.com>
2017-11-15 17:59:12 +0100

    Drag and drop: Standardizing payload types as defines. (#143)

--- src/imgui/imgui_internal.h
+++ src/imgui/imgui_internal.h
@@ -169,6 +169,10 @@ inline void operator delete(void*, ImPlacementNewDummy, void*) {}
 // Types
 //-----------------------------------------------------------------------------
 
+// Drag and Drop payload types. String starting with '_' are managed by Dear ImGui.
+#define IMGUI_PAYLOAD_TYPE_COLOR_4F         "_COL4F"    // float[4]     // Standard type for colors. User code may use this type. Build a float[4] out of a float[3] if you don't have alpha.
+#define IMGUI_PAYLOAD_TYPE_DOCKABLE         "_IMDOCK"   // ImGuiWindow* // [Internal] Docking/tabs
+
 enum ImGuiButtonFlags_
 {
     ImGuiButtonFlags_Repeat                 = 1 << 0,   // hold to repeat
