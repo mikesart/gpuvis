c62086bd742d5b3a9d059bd0fed6cdc30eb65ee7
Michael Sartain <mikesart@fastmail.com>
2017-11-16 15:31:47 -0700

    Add per_cpu stats comments

--- sample/trace-cmd-status.sh
+++ sample/trace-cmd-status.sh
@@ -4,11 +4,33 @@ CMD="trace-cmd stat"
 echo $CMD
 $CMD
 
+#
+# cat /sys/kernel/tracing/per_cpu/cpu0/stats
+#   entries: 5
+#   overrun: 0
+#   commit overrun: 0
+#   bytes: 224
+#   oldest event ts: 96955.082688
+#   now ts: 97157.747457
+#   dropped events: 0
+#   read events: 0
+#
+# Ring buffer stats:
+#   Entries: The number of events that are still in the buffer.
+#   Overrun: The number of lost events due to overwriting when the buffer was full.
+#   Commit overrun: Should always be zero.
+#     This gets set if so many events happened within a nested event (ring buffer is re-entrant),
+#     that it fills the buffer and starts dropping events.
+#   Bytes: Bytes actually read (not overwritten).
+#   Oldest event ts: The oldest timestamp in the buffer.
+#   Now ts: The current timestamp.
+#   Dropped events: Events lost due to overwrite option being off.
+#   Read events: The number of events read.
+#
 if [ -r "/sys/kernel/tracing/per_cpu/cpu0/stats" ]; then
 
 STAT_FILES=()
 
-# /sys/kernel/tracing/per_cpu/cpu0/stats
 for (( i = 0 ; i <= 256 ; i++ )) ; do
     FILE="/sys/kernel/tracing/per_cpu/cpu${i}/stats"
     if [ ! -r ${FILE} ]; then
