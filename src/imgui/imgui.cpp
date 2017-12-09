3127abc2dee08b7b99c483188b64a628b8ece0b2
Branimir Karadžić <branimirkaradzic@gmail.com>
2017-12-09 13:47:54 -0800

    Fixed warning: logical ‘and’ of equal expressions

--- src/imgui/imgui.cpp
+++ src/imgui/imgui.cpp
@@ -4519,7 +4519,7 @@ bool ImGui::Begin(const char* name, bool* p_open, ImGuiWindowFlags flags)
         window->Size = window->Collapsed ? window->TitleBarRect().GetSize() : window->SizeFull;
         if ((flags & ImGuiWindowFlags_ChildWindow) && !(flags & (ImGuiWindowFlags_Popup|ImGuiWindowFlags_Tooltip)))
         {
-            IM_ASSERT(window_size_x_set_by_api && window_size_x_set_by_api); // Submitted by BeginChild()
+            IM_ASSERT(window_size_x_set_by_api && window_size_y_set_by_api); // Submitted by BeginChild()
             window->Size = window->SizeFull;
         }
 
