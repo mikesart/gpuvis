8e739ae39d67f20f84dd99ee31c840c6e004b986
omar <omarcornut@gmail.com>
2017-12-12 23:35:04 +0100

    Added IsAnyWindowFocused() (from Nav branch).

--- src/imgui/imgui.cpp
+++ src/imgui/imgui.cpp
@@ -3401,6 +3401,12 @@ bool ImGui::IsAnyWindowHovered()
     return g.HoveredWindow != NULL;
 }
 
+bool ImGui::IsAnyWindowFocused()
+{
+    ImGuiContext& g = *GImGui;
+    return g.NavWindow != NULL;
+}
+
 static bool IsKeyPressedMap(ImGuiKey key, bool repeat)
 {
     const int key_index = GImGui->IO.KeyMap[key];
