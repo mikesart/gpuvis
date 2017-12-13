40991dc06c41ce807c34a936e47d413eed010476
omar <omarcornut@gmail.com>
2017-12-13 19:26:04 +0100

    Columns: Added internal tracking of a few flag temporarily, to facilitate the work of third-parties who have columns patches. (#125)

--- src/imgui/imgui_internal.h
+++ src/imgui/imgui_internal.h
@@ -423,6 +423,8 @@ struct ImGuiColumnsSet
 {
     ImGuiID             ID;
     ImGuiColumnsFlags   Flags;
+    bool                IsFirstFrame;
+    bool                IsBeingResized;
     int                 Current;
     int                 Count;
     float               MinX, MaxX;
@@ -436,6 +438,8 @@ struct ImGuiColumnsSet
     {
         ID = 0;
         Flags = 0;
+        IsFirstFrame = false;
+        IsBeingResized = false;
         Current = 0;
         Count = 1;
         MinX = MaxX = 0.0f;
