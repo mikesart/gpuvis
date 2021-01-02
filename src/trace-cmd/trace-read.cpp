mikesart/gpuvis

commit 93f2f67f963e8ea8e42c2199fa43c97477d409fd

trace-read: do not modify timestamps in ftrace events
We're about to allow multiple files to be loaded and we need to have a
common time base for all files. Relative timestamps local to a file
won't work.

src/trace-cmd/trace-read.cpp
@@ -1648,7 +1648,7 @@ static int trace_enum_events( trace_data_t &trace_data, tracecmd_input_t *handle
 
         trace_event.pid = pid;
         trace_event.cpu = record->cpu;
-        trace_event.ts = record->ts - trace_data.trace_info.min_file_ts;
+        trace_event.ts = record->ts;
 
         trace_event.comm = strpool.getstrf( "%s-%u", comm, pid );
 

src/gpuvis_ftrace_print.cpp
@@ -295,7 +295,7 @@ void TraceEvents::new_event_ftrace_print( trace_event_t &event )
 
     if ( bufvar == bufvar_ltime )
     {
-        event.ts = atoll( var_ltime ) - m_trace_info.min_file_ts;
+        event.ts = atoll( var_ltime );
 
         // Remove "ltime=XXX", etc from buf
         buf = trim_ftrace_print_buf( newbuf, buf, var_ltime, s_buf_vars[ bufvar ].len );

commit e7fde9ca554fb79cbef9e8088234cb1d7bdec525

trace-read: Fix high-prec vblank timestamps
Do not subtract `min_file_ts` when assigning the high-prec vblank
timestamp.

This fixes a regression of high-prec vblank TS not showing up,
introduced by

    93f2f67f963e8ea8e42c2199fa43c97477d409fd
    trace-read: do not modify timestamps in ftrace events

src/trace-cmd/trace-read.cpp
@@ -1739,7 +1739,7 @@ static int trace_enum_events( trace_data_t &trace_data, tracecmd_input_t *handle
                     unsigned long long val = pevent_read_number( pevent,
                                ( char * )record->data + format->offset, format->size );
 
-                    trace_event.vblank_ts = val - trace_data.trace_info.min_file_ts;
+                    trace_event.vblank_ts = val;
                 }
                 else if ( trace_event.name == trace_data.drm_vblank_event_str &&
                           format_name == trace_data.high_prec_str &&